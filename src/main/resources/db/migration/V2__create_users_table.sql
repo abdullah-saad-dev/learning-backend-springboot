089d269a527aaba5711912f2169a7ebaee6b98bd
Abdullah Saad <abdullah.m.saad@outlook.com>
2026-08-18 15:29:17 +0300

    (feat): add Flyway migrations for users and tasks schemas, update project dependencies for Flyway integration

---
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,18 @@
+create collation case_insensitive(
+    provider = icu,
+    locale = 'und-u-ks-level2',
+    deterministic = false
+    );
+
+
+create table users (
+    id    uuid default uuidv7() primary key,
+    name  varchar(30) not null,
+    email varchar(255) not null collate case_insensitive,
+    password_hash varchar(255) not null,
+    role varchar(255) not null default 'USER',
+    enabled boolean not null default true,
+    created_at timestamptz not null default now(),
+    constraint unique_email unique(email),
+    constraint check_role check(role in ('USER', 'ADMIN'))
+);
